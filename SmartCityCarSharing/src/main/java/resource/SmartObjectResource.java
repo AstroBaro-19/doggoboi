c33f5439d0d6824656de427acee83634678d7c14
Alessandro Baroni <baroni.alessandro99@gmail.com>
2021-08-27 15:46:39 +0200

    newfiles

---
+++ SmartCityCarSharing/src/main/java/resource/SmartObjectResource.java
@@ -0,0 +1,41 @@
+package resource;
+
+public abstract  class SmartObjectResource<T> {
+
+    private String id;
+
+    private String type;
+
+    public SmartObjectResource() {
+    }
+
+    public SmartObjectResource(String id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("SmartObjectResource{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", type='").append(type).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
