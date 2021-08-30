9b03d2edd10c00c86d3ae6aa4a2b2e96a942211d
Alessandro Baroni <baroni.alessandro99@gmail.com>
2021-08-30 22:59:29 +0200

    modelling BatterySensorResource

--- SmartCityCarSharing/src/main/java/resource/SmartObjectResource.java
+++ SmartCityCarSharing/src/main/java/resource/SmartObjectResource.java
@@ -3,6 +3,9 @@ package resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 //Creating generic abstract class
 public abstract  class SmartObjectResource<T> {
@@ -13,14 +16,19 @@ public abstract  class SmartObjectResource<T> {
 
     private String type;
 
+    protected List<ResourceDataListener<T>> resourceListenerList;
+
 
     public SmartObjectResource() {
+        this.resourceListenerList=new ArrayList<>();
 
     }
 
     public SmartObjectResource(String id, String type) {
         this.id = id;
         this.type = type;
+        this.resourceListenerList=new ArrayList<>();
+
     }
 
     public String getId() {
@@ -40,6 +48,32 @@ public abstract  class SmartObjectResource<T> {
     }
 
     public abstract T loadUpdatedValue();
+
+    protected void notifyUpdate(T updatedValue){
+        if (this.resourceListenerList!=null && this.resourceListenerList.size()>0){
+            this.resourceListenerList.forEach(resourceDataListener -> {
+                if (resourceDataListener!=null){
+                    resourceDataListener.onDataChange(resourceDataListener,updatedValue);
+                }
+                else {
+                    logger.error("Nothing to notify ...");
+                }
+            });
+        }
+    }
+
+    public void addDataListener(ResourceDataListener<T> resourceDataListener){
+        if (resourceListenerList!=null){
+            this.resourceListenerList.add(resourceDataListener);
+        }
+    }
+
+    public void removeDataListener(ResourceDataListener<T> resourceDataListener){
+        if (resourceListenerList!=null && resourceListenerList.contains(resourceDataListener)){
+            this.resourceListenerList.remove(resourceDataListener);
+        }
+    }
+
 
     @Override
     public String toString() {
