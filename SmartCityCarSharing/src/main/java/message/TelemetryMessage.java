325cf452f8a021f6b3ba545b5356c706da1b1c72
Alessandro Baroni <baroni.alessandro99@gmail.com>
2021-08-31 18:53:08 +0200

    Structure message implemented

---
+++ SmartCityCarSharing/src/main/java/message/TelemetryMessage.java
@@ -0,0 +1,67 @@
+package message;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @authors - Alessandro Baroni, Simone Brunelli, Riccardo Mari
+ * @project - Smart City Car Sharing
+ */
+
+// Jackson library
+// @JsonProperty: is used to indicate external property name,
+// name used in data format (JSON or one of other supported data formats)
+
+public class TelemetryMessage extends GenericMessageStructure{
+
+    @JsonProperty("timestamp")
+    private long timestamp;
+
+    @JsonProperty("type")
+    private String type;
+
+    @JsonProperty("data")
+    private Object dataValue;
+
+    public TelemetryMessage() {
+    }
+
+    public TelemetryMessage(long timestamp, String type, Object dataValue) {
+        this.timestamp = timestamp;
+        this.type = type;
+        this.dataValue = dataValue;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getDataValue() {
+        return dataValue;
+    }
+
+    public void setDataValue(Object dataValue) {
+        this.dataValue = dataValue;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("TelemetryMessage{");
+        sb.append("timestamp='").append(timestamp).append('\'');
+        sb.append(", type='").append(type).append('\'');
+        sb.append(", dataValue=").append(dataValue);
+        sb.append('}');
+        return sb.toString();
+    }
+}
