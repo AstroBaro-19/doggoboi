92710102687884f80f3a82de6421dfafd2569bd2
Alessandro Baroni <baroni.alessandro99@gmail.com>
2021-09-10 00:37:21 +0200

    totalConsumption is now fixed and deployed in BatteryConsumer

--- SmartCityCarSharing/src/main/java/consumer/DataCollectorTripManagerConsumer.java
+++ SmartCityCarSharing/src/main/java/consumer/DataCollectorTripManagerConsumer.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import resource.BatterySensorResource;
 import resource.GpsGpxSensorResource;
+import utils.GpsConsumption;
 import utils.GpsDistance;
 
 import java.util.*;
